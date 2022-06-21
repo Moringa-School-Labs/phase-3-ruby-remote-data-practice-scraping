6c2de0f1e417656135f336317bc6d7520dc831a7
catherine <vuthi.esther@gmail.com>
2022-06-21 19:32:45 +0300

    completed assignment

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,14 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html)
+
+doc.css(".heading-financier").text.strip
+
+#iterating over elements
+courses = doc.css(".card-link")
+courses.each do |course|
+    puts course.text.strip
+end
+
